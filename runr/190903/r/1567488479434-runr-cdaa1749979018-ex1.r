be0cf5463656d40274cbe85b8b60e916f4911e0b
PT VaRiskindo <id.variskindo@gmail.com>
2019-09-03 13:28:03 +0800

    Data file uploaded (03 Sep 2019 13:27:59)

---
+++ runr/190903/r/1567488479434-runr-cdaa1749979018-ex1.r
@@ -0,0 +1,24 @@
+
+datafolder <- getwd()
+setwd(datafolder)   ## load directory containing files
+datafolder
+
+data <- read.csv("ex1.csv")          ## load data file to RStudio 
+data                                 ## display elements  
+attach(data)                         ## load variable names to memory
+
+## create pie chart
+pie(faculty)                         ## create pie chart with default colours
+pie(faculty, labels = dept, main = "Piechart of Faculty by Department")
+
+## create bar plot
+barplot(faculty)
+barplot(faculty, names.arg = dept, main = "Relative Frequency of Faculty by Department")
+
+## get total number faculties
+sum(faculty)
+
+## get relative frequency
+barplot(faculty/53, names.arg = dept, main = "Barplot of Faculty by Department")
+
+
